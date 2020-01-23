9c298aa1fbe3a1c4bd1bd4550ad5f0adf5cf38b4
Kathryn Wohl <ktwohl@gmail.com>
2020-01-23 16:03:42 -0600

    added functions to return HTML and to add that HTML to the dom

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,7 +1,26 @@
 fetch("http://localhost:8088/food")
     .then(foods => foods.json())
     .then(parsedFoods => {
-        console.table(parsedFoods)
-    })
+        parsedFoods.forEach(food => {
+            console.log(food);
+            const foodAsHTML = foodFactory(food)
+            addFood(foodAsHTML)
+        })
+    });
 
-    
+
+const foodFactory = (food) => {
+    return `
+    <div class="foodCard">
+    <h2>${food.name}</h2>
+    <h4>${food.category}</h4>
+    <h4>${food.ethnicity}</h4>
+    </div>
+    `;
+}
+
+const addFood = (foodHtml) => {
+    const containter = document.querySelector('.foodList');
+    // const foodHtml = foodFactory(food);
+    containter.innerHTML += foodHtml;
+}
